bf102f4af8c33fe8c929c577001061ba4eb6c3bb
TatuWasca <pablorubiolo2004@gmail.com>
2023-01-14 22:36:53 -0300

    Quick fix

---
+++ src/main/java/com/ArgentinaPrograma/ProyectoIntegrador/Security/Controller/AuthController.java
@@ -0,0 +1,85 @@
+package com.ArgentinaPrograma.ProyectoIntegrador.Security.Controller;
+
+import com.ArgentinaPrograma.ProyectoIntegrador.Security.DTO.JwtDto;
+import com.ArgentinaPrograma.ProyectoIntegrador.Security.DTO.LoginUsuario;
+import com.ArgentinaPrograma.ProyectoIntegrador.Security.DTO.NuevoUsuario;
+import com.ArgentinaPrograma.ProyectoIntegrador.Security.Entity.Rol;
+import com.ArgentinaPrograma.ProyectoIntegrador.Security.Entity.Usuario;
+import com.ArgentinaPrograma.ProyectoIntegrador.Security.Enums.RolNombre;
+import com.ArgentinaPrograma.ProyectoIntegrador.Security.JWT.JwtProvider;
+import com.ArgentinaPrograma.ProyectoIntegrador.Security.Service.RolService;
+import com.ArgentinaPrograma.ProyectoIntegrador.Security.Service.UsuarioService;
+import java.util.HashSet;
+import java.util.Set;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = {"https://frontend-4b57b.web.app","https://frontend-4b57b.firebaseapp.com/"})
+public class AuthController {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UsuarioService usuarioService;
+    @Autowired
+    RolService rolService;
+    @Autowired
+    JwtProvider jwtProvider;
+    
+    @PostMapping("/nuevo")
+    public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult){
+        if(bindingResult.hasErrors())
+            return new ResponseEntity(new Mensaje("campos mal puestos"),HttpStatus.BAD_REQUEST);
+        
+        if(usuarioService.exitsByNombreUsuario(nuevoUsuario.getNombreUsuario()))
+            return new ResponseEntity(new Mensaje("Usuario existente"),HttpStatus.BAD_REQUEST);
+        
+        Usuario usuario  = new Usuario(nuevoUsuario.getNombre(),nuevoUsuario.getNombreUsuario(), passwordEncoder.encode(nuevoUsuario.getPassword()));
+        
+        Set<Rol> roles = new HashSet<>();
+        roles.add(rolService.getByRolNombre(RolNombre.ROLE_USER).get());
+        
+        if(nuevoUsuario.getRoles().contains("admin"))
+            roles.add(rolService.getByRolNombre(RolNombre.ROLE_ADMIN).get());
+        usuario.setRoles(roles);
+        usuarioService.save(usuario);
+        
+        return new ResponseEntity(new Mensaje("Usuario guardado"), HttpStatus.CREATED);
+    }
+    
+    @PostMapping("/login")
+    public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
+        if(bindingResult.hasErrors())
+            return new ResponseEntity(new Mensaje("campos mal puestos"),HttpStatus.BAD_REQUEST);
+        
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+        loginUsuario.getNombreUsuario(), loginUsuario.getPassword()));
+        
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        
+        String jwt = jwtProvider.generateToken(authentication);
+        
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        
+        JwtDto jwtDto = new JwtDto(jwt, userDetails.getUsername(), userDetails.getAuthorities());
+        
+        return new ResponseEntity(jwtDto, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/ArgentinaPrograma/ProyectoIntegrador/Security/Entity/Usuario.java
@@ -0,0 +1,45 @@
+package com.ArgentinaPrograma.ProyectoIntegrador.Security.Entity;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Column(unique = true)
+    private String nombre;
+    @NotNull
+    private String nombreUsuario;
+    @NotNull
+    private String password;
+    
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "usuario_rol", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "rol_id"))
+    private Set<Rol> roles = new HashSet<>();
+
+    public Usuario() {
+    }
+    
+    public Usuario(String nombre, String nombreUsuario, String password) {
+        this.nombre = nombre;
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+    }
+    
+}
